d71d6de87a3e34b9e5ea0e31b6f61abdb329a9e5
EliasVincent <riesyeti@outlook.de>
2021-10-01 11:59:10 +0200

    self signed certificate

--- db.js
+++ db.js
@@ -14,7 +14,9 @@ const devConfig = {
 const prodConfig = {
     // coming from a heroku postgres addon
     connectionString: process.env.DATABASE_URL + "?ssl=true",
+    ssl: { rejectUnauthorized: false }
 }
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? prodConfig : devConfig);
 module.exports = pool;
+
