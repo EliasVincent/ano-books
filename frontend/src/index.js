00de17f1d71af5d652f38c98980c7887311e70b3
Elias Vincent Simon <riesyeti@outlook.de>
2021-10-10 00:09:02 +0200

    turn into PWA

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Book from './components/Book';
@@ -43,3 +44,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorker.register();
